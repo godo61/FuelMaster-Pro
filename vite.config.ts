2c5bb5df9b0d77a93f656ea9429486bd873835b7
godo61 <64096643+godo61@users.noreply.github.com>
2025-12-30 07:43:27 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Ajustado específicamente para el repositorio en GitHub
-  base: '/FuelMaster-Pro/',
+  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
